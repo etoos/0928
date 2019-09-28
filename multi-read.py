9f6588948345e5e26021e9147f392cc22556ca77
etoos <49575896+etoos@users.noreply.github.com>
2019-09-28 12:28:30 +0900

    Add files via upload

---
+++ multi-read.py
@@ -0,0 +1,8 @@
+import sys
+import glob
+import os
+
+for input_file in glob.glob(os.path.join(".", '*.txt')):
+     with open(input_file, 'r') as filereader:
+        for row in filereader:
+            print("{}".format(row.strip()))
